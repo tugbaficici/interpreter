62a6322bbf2c40ca435d701b5677909057549925
Zeki Ahmet Bayar <zekiahmetbayar1@gmail.com>
2021-05-08 21:43:34 +0300

    k r fonksiyonlari eklendi

--- yorumlayici.py
+++ yorumlayici.py
@@ -1,23 +1,22 @@
-
 inputString = "n=0;{n-2*5?<n;n=n+1;}"
-
-stack = []
+islemlerStack = ['$']
+stack = ['$']
 
 # Main buradan başlamaktadır.
-inputToStack()
-token = getToken()#global
-P()
+inputToStack() # Global tanimlama
+token = getToken() 
+islemlerStack.append(token)
 
+sonuc = P()
+print(sonuc)
 
 def inputToStack():
     for i in reversed(range(len(inputString))):
         stack.append(inputString[i])
 
-
 def getToken():
     top = stack.pop()
     return top
-
 
 def P():
     if token == ".":
@@ -26,6 +25,7 @@ def P():
     else:
         C()  # geri döndüğünde token değişkeninde nokta olmalı global token?
 
+    P()
 
 def C():
     # işlem bittiğinde return .
@@ -33,20 +33,31 @@ def C():
     if token == "[":
         token=getToken()
         I()
+
     elif token == "{":
         W()
+        cikti += '+' 
+
     elif token == "<":
         cikis()
+
     elif token == ">":
         G()
-    else:  # token harf ise
+
+    elif K(token) == True:  # token harf ise
         A()
     
-    #token="."
+    elif token == '$':
+        token == '.'
+        P()
+
+    else:
+        return "Hata !"
 
 def I():
     # if
     # I   → '[' E '?'  C{C} ':' C{C} ']' | '[' E '?' C{C} ']'
+
     E()#burada her türlü e çağrılacak iki ihtimal için
 
 
@@ -81,23 +92,50 @@ def T():
 
 def U():
     # U → F '^' U | F
-    F()
+    degisken = F()
 
+n = 0
+K = E
+A  
+C
+P
 def F():
     # F → '(' E ')' | K | R
+    global token
+
     if token == "(":
         E()
-    elif token harf ise:
-        K()
-    elif token rakam ise:
-        R()
 
-def K():
+    elif K(token) == True:
+        return 'K'
+
+    elif R(token) == True:
+        return 'R'    
+    
+    else:
+        return "Hata ! Gecersiz string !"
+
+def K(harf):
     # harf
     # K → 'a'  |  'b'  | … |  'z'
+    kucukHarflerListesi = ['a','b','c','d','e','f','g',
+                           'h','i','j','k','l','m','n',
+                           'o','p','r','s','t','u','v',
+                           'y','z','x','w','q']
 
+    if harf in kucukHarflerListesi:
+        return True
+    
+    else:
+        return False
 
-
-def R():
+def R(rakam):
     # rakam
     # R → '0'  |  '1'  | … |  '9'
+    rakamlarListesi = [0,1,2,3,4,5,6,7,8,9]
+
+    if rakam in rakamlarListesi:
+        return True
+    
+    else:
+        return False
